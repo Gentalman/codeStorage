3ae8f916aae318157dcfee72f84705da7448b2d8
gentaldu <gentaldu@gmail.com>
2014-11-03 22:21:23 +0800

    create a table view

--- Qtdemo/databaseDriver/databaseDriver/main.cpp
+++ Qtdemo/databaseDriver/databaseDriver/main.cpp
@@ -3,41 +3,54 @@
 #include <QStringList>
 #include <QVariant>
 #include <QMessageBox>
+#include <QTableView>
 
 #include <QSqlDatabase>
 #include <QSqlQuery>
+#include <QSqlQueryModel>
 
 static bool createConnection()
 {
     QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
-    db.setDatabaseName(":memory:");
+    db.setDatabaseName("checkOn.db");
     if (!db.open()) {
         QMessageBox::critical(0, "Cannot open database",
             "Unable to establish a database connection.", QMessageBox::Cancel);
         return false;
     }
+
     QSqlQuery query;
-    query.exec("create table student (id int primary key, "
-               "name varchar(20))");
-    query.exec("insert into student values(0, 'Gental')");
-    query.exec("insert into student values(1, 'LiuTao')");
-    query.exec("insert into student values(2, 'WangHong')");
+    query.exec("create table student (name varchar(20) primary key, "
+               "time int)");
+    query.exec("insert into student values('Gental', 0)");
+    query.exec("insert into student values('LiuTao', 1)");
+    query.exec("insert into student values('WangHong', 2)");
     return true;
 }
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    int numRows;
 
     // 创建数据库连接
-    if (!createConnection()) return 1;
+    if(!createConnection()) return 1;
     // 使用QSqlQuery查询整张表
     QSqlQuery query;
     query.exec("select * from student");
     while(query.next())
     {
-        qDebug() << query.value(0).toInt() << query.value(1).toString();
+        qDebug() << query.value(0).toString() << query.value(1).toInt();
     }
+
+    QSqlQueryModel *model = new QSqlQueryModel();
+    model->setQuery("select * from student");
+    model->setHeaderData(0, Qt::Horizontal, QString::fromUtf8("名字"));
+    model->setHeaderData(0, Qt::Horizontal, QString::fromUtf8("签到次数"));
+
+    QTableView *view = new QTableView();
+    view->setModel(model);
+    view->show();
 
     return a.exec();
 
